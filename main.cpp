9833a1577cf7c011879655eed388c790bd49ae9d
PatelParth1085 <202304008@daiict.ac.in>
2024-02-28 00:47:48 +0530

    28/02/2024

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-class Inmates{
-    //properties
-   private:
-   string id;
-   int sleeptime;
+class Inmates
+{
+    // properties
+    private:
+    string id;
+    int sleeptime;
 
-    
-
-    
+    // constructers
+    public:
+    Inmates(string id) : id(id) {}
 };
-int main(){
+int main()
+{
 
-return 0;
+    return 0;
 }
