bbc839a8396a029c2de6df4c834f8609686bf6c3
JITEN-BHARGA <160772021+JITEN-BHARG@users.noreply.github.com>
2024-03-02 23:41:18 +0530

    clean

--- main.cpp
+++ main.cpp
@@ -14,13 +14,10 @@ class Sleepinducers{
     vector<vector<int>> dorms; //inmates assigned in dorms(rooms)
     
     public:
-<<<<<<< HEAD
-=======
 
     Sleepinducers(int ti,int td,double mst) 
     : totalinmates(ti),totaldorms(td),maxsleeptime(mst){}
     
->>>>>>> afbde7d5693ec0a49faffa41ecd50daf6687c254
 };
 
 int main()
