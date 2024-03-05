3ef22d170615f27840bf4f09404aaec6fd32b243
JITEN-BHARGA <160772021+JITEN-BHARG@users.noreply.github.com>
2024-03-05 18:36:07 +0530

    error update

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ private:
     vector<vector<int>> dorms; // inmates assigned in dorms(rooms)
 
 public:
-    Sleepinducers(int ti, int tdb) : totalinmates(ti), totaldorms(td) {}
+    Sleepinducers(int ti, int tdb) : totalinmates(ti), totaldorms(tdb) {}
 
     void initializeinmates()
     {
