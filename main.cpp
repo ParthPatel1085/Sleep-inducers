7022dd7c10c5a1a2c0ce3992491997e207468ed1
PatelParth1085 <202304008@daiict.ac.in>
2024-03-08 12:03:39 +0530

    input txt file

--- main.cpp
+++ main.cpp
@@ -1,77 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <ctime>
-
+#include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
-
-class Sleepinducers
+class Inmate
 {
-    // properties
 private:
-    int totalinmates;
-    int totaldorms;
+    string inmate_name;
+    string earpod_ID;
+    vector<int> sleep_times;  
+    int fall_asleeptime; 
+    bool earpodactive;
 
-    vector<int> inmates;       // inmates sleeptime in minutes
-    vector<vector<int>> dorms; // inmates assigned in dorms(rooms)
-
-public:
-    Sleepinducers(int ti, int tdb) : totalinmates(ti), totaldorms(tdb) {
-        dorms.resize(totaldorms);
-    }
-
-    void initializeinmates()
+public: 
+    Inmate(string name, string earpodID, vector<int> sleeptimes, int fallasleeptime)
     {
-        srand(time(0));
-        for (int i = 0; i < totalinmates; i++)
-        {
-            inmates.push_back(rand() % (60));
-        }
-    }
-
-    void Dorms()
-    {
-        vector<int> time(totaldorms, 0);
-        int k = 60 / totaldorms;
-        int s = 0;
-        for (int i = 0; i < totaldorms; i++)
-        {
-            time[i] = k;
-            k += time[0];
-            time[totaldorms - 1] = 60;
-            for (int j = 0; j < totalinmates; j++)
-            {
-                if (inmates[j] <= time[i] && inmates[j] > s)
-                {
-                    dorms[i].push_back(inmates[j]);
-                }
-            }
-            s = time[i];
-        }
-    }
-    void display()
-    {
-
-        for (int i = 0; i < totaldorms; i++)
-        {
-            for (auto &r : dorms[i])
-            {
-                cout << r << "   ";
-            }
-            cout << endl;
-        }
+        inmate_name = name;
+        earpod_ID = earpodID;
+        sleep_times = sleeptimes;
+        fall_asleeptime = fallasleeptime;
+        earpodactive = true; 
     }
 };
+class Dorm
+{
+private:
+    string dorm_name;
+    vector<Inmate> assigned_inmate; 
+    set<int> avail_channel;     
+    set<int> play_channel;      
+
+public: 
+    Dorm(string name) : dorm_name(name), assigned_inmate(), avail_channel(), play_channel() {}
+};
+
 int main()
 {
-    int M;
-    int N;
-    int P;
-    Sleepinducers A(20, 7);
-    A.initializeinmates();
-    A.Dorms();
-    A.display();
-
     return 0;
 }
-
-
