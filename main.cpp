c16986c89af97faedf79ba2130243bbfa92cb889
JAIMEENCHAUHAN <161302425+JAIMEENCHAUHAN@users.noreply.github.com>
2024-03-11 19:34:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,55 @@ public:
     int fall_asleeptime; 
     bool earpodactive;
 
+    void take_input_file(int line_number)
+    {
+        ifstream file("data.csv");
+        if (!file.is_open())
+        {
+            cout << "Error while opening file" << endl;
+        }
+        else
+        {
+            string line;
+            for (int i = 1; i <= line_number; i++)
+            {
+                if (!getline(file, line))
+                {
+                    cout << "Error: Not enough lines in file" << endl;
+                    file.close();
+                    return;
+                }
+            }
+            istringstream iss(line);
+            vector<string> tokens;
+            string token;
+            while (getline(iss, token, ','))
+            {
+                tokens.push_back(token);
+            }
+            if (tokens.size() >= 10)
+            {
+                inmate_name = tokens[0];
+                earpod_ID = tokens[1];
+
+                    fall_asleeptime = stoi(tokens[9]);
+                    for (int j = 2; j < 9; j++)
+                    {
+                        {
+                            sleep_times.push_back(stoi(tokens[j]));
+                        }
+                    }
+
+                    for (const auto &value : tokens)
+                    {
+                        cout << value << "   ";
+                    }
+                    cout << endl;
+                
+            }
+            file.close(); // Closing the file after reading
+        }
+    }
 
 };
 class Dorm
