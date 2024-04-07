b6033b39345e5d23585e53641c306481bfc78438
PatelParth1085 <202304008@daiict.ac.in>
2024-04-07 17:07:28 +0530

    .

--- Dorms.cpp
+++ Dorms.cpp
@@ -4,8 +4,6 @@ class Dorm
 public:
   string dorm_name;
   vector<Inmate> assigned_inmates;
-  set<int> avail_channel;
-  set<int> play_channel;
   int max_sleep_times;
 
   Dorm(const string &name, int p) : dorm_name(name), max_sleep_times(p) {}
